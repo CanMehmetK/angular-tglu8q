2c2ddf63db4f0159bba7c8f3faa270bc73dc1103
CanMehmetK <kanpinar@hotmail.com>
2020-05-09 07:13:33 +0300

    ..

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,7 @@ import './polyfills';
 
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
-
 platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
   // Ensure Angular destroys itself on hot reloads.
   if (window['ngRef']) {
