64f960d78f02a2d644c33b1a6a419ae7db895875
CanMehmetK <kanpinar@hotmail.com>
2020-05-09 04:11:38 +0300

    1 file modified, 2 added

---
+++ src/app/mikro-kargo/firma/firma-ekle/firma-ekle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firma-ekle',
+  templateUrl: './firma-ekle.component.html',
+  styleUrls: ['./firma-ekle.component.css']
+})
+export class FirmaEkleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/mikro-kargo/firma/firma.module.ts
+++ src/app/mikro-kargo/firma/firma.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { FirmaEkleComponent } from './firma-ekle/firma-ekle.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: []
+  declarations: [FirmaEkleComponent]
 })
 export class FirmaModule {}
