92ec8dfc4f76a0c14a40f1f7af52c7f763d0285a
CanMehmetK <kanpinar@hotmail.com>
2020-05-09 03:54:44 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
-
-
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule,RouterModule, FormsModule ],
   declarations: [ AppComponent,   ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/mikro-kargo/mikro-kargo.module.ts
+++ src/app/mikro-kargo/mikro-kargo.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { RouterModule, Routes } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from "@angular/router";
+import { LayoutComponent } from "./layout/layout.component";
 
 const routes: Routes = [
-    {
-        path: '', component: LayoutComponent, children: []}};
+  {
+    path: "",
+    component: LayoutComponent,
+    children: []
+  }
+];
 @NgModule({
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   declarations: []
 })
-export class MikroKargoModule { }
+export class MikroKargoModule {}
