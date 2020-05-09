b2b28cd5ce96c1548e7a980ffac63bce710bc5ca
CanMehmetK <kanpinar@hotmail.com>
2020-05-09 05:21:56 +0300

    1 file modified, 1 added

--- src/app/mikro-kargo/firma/firma-kullanici-listesi/firma-kullanici-listesi.component.ts
+++ src/app/mikro-kargo/firma/firma-kullanici-listesi/firma-kullanici-listesi.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-firma-kullanici-listesi',
-  templateUrl: './firma-kullanici-listesi.component.html',
-  styleUrls: ['./firma-kullanici-listesi.component.css']
+  selector: "app-firma-kullanici-listesi",
+  templateUrl: "./firma-kullanici-listesi.component.html",
+  styleUrls: ["./firma-kullanici-listesi.component.css"]
 })
 export class FirmaKullaniciListesiComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
